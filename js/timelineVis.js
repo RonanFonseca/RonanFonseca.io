be6dcf0b5df478a0b9b748ba90eb8315e41ccf80
RonanFonseca <54601025+RonanFonseca@users.noreply.github.com>
2022-11-19 16:19:54 -0300

    Chumbox test
    Added chumbox vis folder

--- js/timelineVis.js
+++ js/timelineVis.js
@@ -15,7 +15,7 @@ class TimelineVis {
     initVis() {
         let vis = this;
 
-        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
+        vis.margin = {top: 20, right: 20, bottom: 5, left: 20};
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
