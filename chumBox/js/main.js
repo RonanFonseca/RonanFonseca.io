9353f777e8d1f67eb57e45ce274418c098d3b0f5
RonanFonseca <54601025+RonanFonseca@users.noreply.github.com>
2022-11-20 17:32:19 -0300

    First Commit of Grouped Project
    First commit of grouped project

--- chumBox/js/main.js
+++ chumBox/js/main.js
@@ -6,8 +6,13 @@ facts = [
         fact: "the two main companies behind this sort of advertisement," +
             " taboola and Outbrain, made almost" +
             " <span class=\"highlight\">2.4 billion dollars in revenue</span> in 2021?",
-        sources: ["Taboola earnings report","https://www.sec.gov/ix?doc=/Archives/edgar/data/0001840502/000114036122010978/brhc10035439_20f.htm",
-        "https://investors.outbrain.com/node/7086/html"]},
+        sources:
+            [
+                "Taboola earnings report for 2021",
+            "https://www.sec.gov/ix?doc=/Archives/edgar/data/0001840502/000114036122010978/brhc10035439_20f.htm",
+            "Outbrain earnings report for 2021",
+                "https://investors.outbrain.com/node/7086/html"
+            ]},
     {
         name: "pants.jpg",
         fact: "the U.S. Federal Trade Commission found that these weight-loss ads" +
@@ -20,11 +25,16 @@ facts = [
         fact: "a 2018 study on Twitter data found that the top 1% of fake news spread to between" +
             " <span class=\"highlight\">1000 to 100,000 people</span>, but that the top 1% of true news rarely spread" +
             " to more than <span class=\"highlight\">  1000 people? </span>",
-        sources: ["Prepared Statement of the Federal Trade Commission",
-        "https://www.ftc.gov/system/files/documents/public_statements/316321/140617falsedecepweightloss.pdf"]
+        sources: ["The spread of true and false news online",
+        "https://www.science.org/doi/10.1126/science.aap9559"]
     }
 ]
 
 d3.selectAll(".chumbox").data(facts).on("click", (event, d)=> {
     document.getElementById("factline").innerHTML =`<p>...${d.fact}</p>`;
+    // d.sources.forEach((e, i) => {
+    //     if (i % 2 == 0){
+    //         <a "href = "></a>;
+    //     }
+    // });
 })
